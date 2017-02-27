ea25c4efdbdfcd70dc017da70360bc1524ba9864
Janjaap Bos <janjaap.bos@capitar.com>
2017-02-27 11:49:09 +0000

    Wait after openining publisher socket
    Otherwise published messages are randomly lost

--- examples/pub_sub/simple_publisher.py
+++ examples/pub_sub/simple_publisher.py
@@ -1,6 +1,10 @@
 from nanoservice import Publisher
-
+import time
 
 p = Publisher('ipc:///tmp/pubsub-service.sock')
+
+# Need to wait a bit to prevent lost messages
+time.sleep(0.001)
+
 p.publish('log_line', 'hello world')
 p.publish('cap_line', 'this is uppercase')
